50581f810272d201ef5620bcc454cbc187450f9e
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-07 15:16:52 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :boooty
+
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:type]
+    @height = args[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
