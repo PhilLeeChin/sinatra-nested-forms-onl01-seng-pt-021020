1aca75cf18ecf42d99ee07eedfde3f294e12145c
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-07 16:03:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,14 +9,14 @@ module FormsLab
     end
 
     get '/new' do
-      erb :'/pirates/new'
+      erb :'pirates/new'
     end
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
       @ship_1 = Ship.new(params[:pirate][:ships][0])
       @ship_2 = Ship.new(params[:pirate][:ships][1])
-      erb :'/pirates/show'
+      erb :'pirates/show'
     end
   end
 end
